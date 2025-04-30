f2c32f2871b1902a6764c385befde5d613244423
Axel Leon <axeleon76@gmail.com>
2025-04-30 14:50:01 +0200

    Añadido cambio de estados de autenticacion

--- lib/features/auth/presentation/providers/auth_service.dart
+++ lib/features/auth/presentation/providers/auth_service.dart
@@ -1,57 +1,90 @@
 import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:trixo_frontend/features/auth/presentation/providers/auth_status_provider.dart';
 
-class AuthService{
+class AuthService {
 
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
-
-  Future<void> signUp({ //Registro
-    required String email, 
+  final AuthState _authState = const AuthState();
+  Future<bool> signUp({
+    //Registro
+    required String email,
     required String password,
-    }) async {
-      try{
-        final credentials = await _firebaseAuth.createUserWithEmailAndPassword(
+  }) async {
+    try {
+      final credentials = await _firebaseAuth.createUserWithEmailAndPassword(
           email: email, password: password);
-
-        await credentials.user!.sendEmailVerification();
-
-        log("Usuario registrado: ${credentials.user?.uid}", name: "AuthService");
-
-      } catch (e){
-        log("Error al registrar: $e");
-      }
+      _authState.copyWith(
+        authStatus: AuthStatus.authenticated,
+        userId: credentials.user?.uid,
+      );
+      await credentials.user!.sendEmailVerification();
+      log("Usuario registrado: ${credentials.user?.uid}", name: "AuthService");
+      return true;
+    } catch (e) {
+      _authState.copyWith(
+        authStatus: AuthStatus.notAuthenticated,
+        userId: null
+      );
+      log("Error al registrar: $e");
+      return false;
+    }
   }
 
-  Future<bool> signIn({ //Login
-    required String email, 
+  /// Recarga usuario y comprueba verificación
+  Future<bool> isEmailVerified() async {
+    final user = _firebaseAuth.currentUser;
+    await user?.reload();
+    return user?.emailVerified ?? false;
+  }
+
+  Future<bool> signIn({
+    //Login
+    required String email,
     required String password,
-    }) async {
-      try{
-        final credentials = await _firebaseAuth.signInWithEmailAndPassword(
+  }) async {
+    try {
+      final credentials = await _firebaseAuth.signInWithEmailAndPassword(
           email: email, password: password);
 
-        if(credentials.user == null){
-          log("Usuario no encontrado: $email", name: "AuthService");
-          return false;
-        }
-
-        if(credentials.user?.emailVerified == false){
-          await credentials.user?.sendEmailVerification();
-          log("Se ha enviado un correo de verificacion a ${credentials.user?.email}", name: "AuthService");
-        }
-
-        log("Se ha iniciado sesion con exito: ${credentials.user?.uid}", name: "AuthService");
-        return true;
-      } catch (e){
-        log("Error al iniciar sesion: $e", name: "AuthService");
+      if (credentials.user == null) {
+        _authState.copyWith(
+          authStatus: AuthStatus.notAuthenticated,
+          userId: null
+        );
+        log("Usuario no encontrado: $email", name: "AuthService");
         return false;
       }
+
+      if (credentials.user?.emailVerified == false) {
+        await credentials.user?.sendEmailVerification();
+        log("Se ha enviado un correo de verificacion a ${credentials.user?.email}",
+            name: "AuthService");
+      }
+
+      _authState.copyWith(
+        authStatus: AuthStatus.authenticated,
+        userId: credentials.user?.uid
+      );
+
+      log("Se ha iniciado sesion con exito: ${credentials.user?.uid}",
+          name: "AuthService");
+      return true;
+    } catch (e) {
+      _authState.copyWith(
+        authStatus: AuthStatus.notAuthenticated,
+        userId: null
+      );
+      log("Error al iniciar sesion: $e", name: "AuthService");
+      return false;
+    }
   }
 
-  Future<bool> signInWithGoogle() async { //Login con google
-    try{
+  Future<bool> signInWithGoogle() async {
+    //Login con google
+    try {
       final GoogleSignInAccount? user = await _googleSignIn.signIn();
 
       final GoogleSignInAuthentication? googleAuth = await user?.authentication;
@@ -60,43 +93,55 @@ class AuthService{
         accessToken: googleAuth?.accessToken,
         idToken: googleAuth?.idToken,
       );
+      
+      final userCredential =
+          await _firebaseAuth.signInWithCredential(credential);
 
-      final userCredential = await _firebaseAuth.signInWithCredential(credential);
-        log("Usuario registrado", name: "AuthService");
-        return userCredential.user != null;
+      _authState.copyWith(
+        authStatus: AuthStatus.authenticated,
+        userId: userCredential.user?.uid
+      );
 
+      log("Usuario registrado", name: "AuthService");
+      return userCredential.user != null;
     } catch (e) {
+      _authState.copyWith(
+        authStatus: AuthStatus.notAuthenticated,
+        userId: null
+      );
       log("Error al iniciar sesion con Google: $e", name: "AuthService");
       return false;
     }
-
   }
 
-  Future<bool> isEmailVerified() async { //Comprobar si se ha verificado
-    final user = _firebaseAuth.currentUser;
-    await user?.reload();
-    return user?.emailVerified ?? false;
-  }
-
-  Future<void> resetPassword({ //Recuperar la contraseña
+  Future<bool> resetPassword({
+    //Recuperar la contraseña
     required String email,
   }) async {
     try {
       await _firebaseAuth.sendPasswordResetEmail(email: email);
-      log("Se ha enviado un correo de recuperacion de contraseña a $email", name: "AuthService");  
+      log("Se ha enviado un correo de recuperacion de contraseña a $email",
+          name: "AuthService");
+      return true;
     } catch (e) {
-      log("Error al enviar el correo de recuperacionde contraseña: $e", name: "AuthService");
+      log("Error al enviar el correo de recuperacionde contraseña: $e",
+          name: "AuthService");
+      return false;
     }
   }
 
-  Future<void> signOut() async { //Logout
+  Future<void> signOut() async {
+    //Logout
     await _firebaseAuth.signOut();
     await _googleSignIn.signOut();
+    _authState.copyWith(
+      authStatus: AuthStatus.notAuthenticated,
+      userId: null
+    );
     log("Usuario desconectado", name: "AuthService");
   }
 
   User? get currentUser {
     return _firebaseAuth.currentUser;
   }
-
 }
