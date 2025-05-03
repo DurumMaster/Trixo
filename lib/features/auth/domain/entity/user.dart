f65341296e4f4c16693c82f329af1b878b428b3b
Carlos <tarea2git@gmail.com>
2025-05-03 13:23:12 +0200

    Carga de Post con API - cambio entity y mapper User

--- lib/features/auth/domain/entity/user.dart
+++ lib/features/auth/domain/entity/user.dart
@@ -1,14 +1,14 @@
 class User {
-  final int id;
-  final String avatarImg;
+  final String id;
   final String username;
   final String email;
-  final String token;
+  final String avatarImg;
+  final String registrationDate;
 
   User(
       {required this.id,
       required this.avatarImg,
       required this.username,
       required this.email,
-      required this.token});
+      required this.registrationDate});
 }
--- lib/features/auth/infrastructure/mapper/user_mapper.dart
+++ lib/features/auth/infrastructure/mapper/user_mapper.dart
@@ -2,10 +2,10 @@ import 'package:trixo_frontend/features/auth/domain/auth_domain.dart';
 
 class UserMapper {
   static User userJsonToEntity(Map<String, dynamic> json) => User(
-        id: json['id'] ?? 0,
-        avatarImg: json['avatar_img'] ?? '',
-        username: json['username'] ?? '',
-        email: json['email'] ?? '',
-        token: json['token'] ?? '',
+        id: json['id'] as String,
+        username: json['username'] as String,
+        email: json['email'] as String,
+        avatarImg: json['avatar_img'] as String,
+        registrationDate: json['registration_date'] as String,
       );
 }
